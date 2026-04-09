69dd9374f69465e5db565fad524163b415673432
nemeco <146324049+ClementMONDARY@users.noreply.github.com>
2026-04-09 19:55:07 +0200

    Revert "no more motion divs for clean screenshot"

--- src/sections/Competences.tsx
+++ src/sections/Competences.tsx
@@ -23,7 +23,7 @@ export default function Competences() {
       id="competences"
       className="min-h-screen py-20 px-8 overflow-hidden bg-gradient-to-tr from-purple-950/50 via-blue-950/50 to-purple-950/50 border-t border-gray-700/50"
     >
-      <div
+      <motion.div
         className="max-w-4xl mx-auto"
         initial={{ opacity: 0 }}
         whileInView={{ opacity: 1 }}
@@ -35,7 +35,7 @@ export default function Competences() {
         </h2>
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {skills.map((skill, index) => (
-            <div
+            <motion.div
               key={skill}
               className="p-6 relative text-center"
               initial={{ opacity: 0, scale: 0.8 }}
@@ -72,10 +72,10 @@ export default function Competences() {
                   index % 2 === 0 ? "bg-gradient-to-l" : "bg-gradient-to-r"
                 } from-indigo-500 to-pink-500 opacity-20 transform rotate-2 shadow-2xl`}
               />
-            </div>
+            </motion.div>
           ))}
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 }
